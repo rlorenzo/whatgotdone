88ec29d9085ff982d1c797ba337b0a266fd3d553
Michael Lynch <git@mtlynch.io>
2019-03-25 17:29:55 -0400

    Temp

---
+++ client/src/components/Journal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="journal">
+    <div class="journalEntry">
+      <div class="date">{{ entry.date }}</div>
+      <div class="journalBody">{{ entry.markdown }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Journal",
+  props: {
+    msg: Array
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
